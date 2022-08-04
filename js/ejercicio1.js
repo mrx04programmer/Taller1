f00ef04e0e7cf6b3295c5b954412619798cd2e87
andres espinel <nelsonandres833@gmail.com>
2022-08-04 11:49:43 -0500

    Ejercicio 1 Javascript

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,3 +1,3 @@
 let nombre = prompt('Digite su nombre:');
 let apellido = prompt('Digite su apellido:')
-alert(`Nombre en mayúsculas: ${nombre.toUpperCase()}\n Su longitud es de ${nombre.length}\n Apellido en minuscula: ${apellido.toLowerCase()}\n Primera letra del nombre:${nombre.charAt(0)}\n Unión: ${nombre.charAt(0)}${apellido}`);
+alert(`Nombre en mayúsculas: ${nombre.toUpperCase()}\n Su longitud es de ${nombre.length}\n Apellido en minuscula: ${apellido.toLowerCase()}\n Primera letra del nombre:${nombre.charAt(0)}\n Unión: ${nombre.charAt(0)}${apellido}`); 
