aa620e49b32de46c24a417f9f9531a83100fe32d
andres espinel <nelsonandres833@gmail.com>
2022-08-04 12:01:52 -0500

    Ejercicio While

---
+++ js/ejerciciowhile.js
@@ -0,0 +1,10 @@
+let r = true;
+
+/* while(r==true){
+    alert("Hola soy juan");
+    r = window.confirm("Desea continuar :");
+} */
+do{
+    alert("Hola soy juan");
+    r = confirm("Desea continuar: ");
+}while(r==true);
