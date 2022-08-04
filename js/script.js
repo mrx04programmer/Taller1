f953e6f587ee57a26ddc9ca8ebbee79eb6fd7d85
andres espinel <nelsonandres833@gmail.com>
2022-08-04 08:33:38 -0500

    Scripting Javascript - Variables

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//console.log("Hello World");
+let numero=25;
+// Primera manera para mostrar texto en consola con concatenación
+console.log('El numero es :',numero);
+// Segunda manera con plantilla literalcon tildes invertidas (Alt+96) Interpolando cadenas
+console.log(`El numero es : ${numero}`);
+
+let nombre="Juanito";
+console.log(`El nombre es ${nombre}`);
+
+let estado=true;
+console.log(`Mi estado es ${estado}`);
+
+let vacia;
+console.log(`Variable nueva: ${vacia}`);
+
+let res=numero+5;
+console.log(`El resultado es :${res}`);
